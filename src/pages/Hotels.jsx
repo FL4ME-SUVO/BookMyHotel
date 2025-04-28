7de6127c0fa8dba40b35938e6a2113e47fe5f55b
PlanCraft <shuvogit9932github@gmail.com>
2025-04-28 21:21:35 +0530

    Search Filter Hotels added

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -3,16 +3,18 @@ import Navbar from '../components/Navbar/Navbar';
 import Hero2 from '../components/Hero/Hero2';
 import Hotel from '../components/Hotels/Hotels'
 import Footer from '../components/Footer/Footer';
+import FilterBar from '../components/FilterBar/FilterBar';
 
 function Hotels() {
     return (
         <div className="about-page">
             <Navbar />
-            <Hero2 
-                backgroundImage="/images/bg_3.jpg" 
-                breadcrumb="Home / Hotels" 
-                title="Hotels" 
+            <Hero2
+                backgroundImage="/images/bg_3.jpg"
+                breadcrumb="Home / Hotels"
+                title="Hotels"
             />
+            <FilterBar />
             <Hotel />
             <Footer />
         </div>
